dbd1712ea243f7942d97d23284a39d336f207b7f
tremarkley <tremarkley@gmail.com>
2018-01-15 12:03:28 -0800

    remove console.log

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,7 +4,6 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  console.log(obj);
   // your code goes here
   var objType = typeof obj; 
   if (objType === 'number' || obj === null || objType === 'boolean') {
