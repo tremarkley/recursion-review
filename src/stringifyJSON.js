0a488799f360353d25aa85fa29d761abce54e661
tremarkley <tremarkley@gmail.com>
2018-01-15 11:32:23 -0800

    Implement stringifyJSON all tests pass

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  console.log(obj);
   // your code goes here
+  var objType = typeof obj; 
+  if (objType === 'number' || obj === null || objType === 'boolean') {
+    return '' + obj;
+  } else if (objType === 'string') {
+    return '\"' + obj + '\"';
+  } else if (Array.isArray(obj)) {
+    let resArray = '[';
+    for (let i = 0; i < obj.length; i++) {
+      resArray += stringifyJSON(obj[i]);
+      if (i < obj.length - 1) {
+        resArray += ',';
+      }
+    }
+    return resArray + ']'; 
+  } else if (objType === 'object') {
+    let resObject = '{';
+    let notEmpty = false;
+    for (let key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        notEmpty = true;
+        resObject += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+      }
+    }
+    if (notEmpty) {
+      resObject = resObject.slice(0, resObject.length - 1);
+    }
+    return resObject + '}';
+  }
 };
