f21421d551b9b21155d12528c721679b48d843f3
tremarkley <tremarkley@gmail.com>
2018-01-15 20:32:46 -0800

    parseJSON for arrays working

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,44 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  // let resultObj;
+
+  if (json[0] === '[') {
+    let resultArr = [];
+    let commaIndex = 1;
+    for (let i = 1; i < json.length; i++) {
+      let curSection;
+      if (json[i] === '[') {
+        // resultArr.push(parseJSON(json.slice(i)));
+
+        for (let j = i; j < json.length; j++) {
+          if (json[j] === ']') {
+            resultArr.push(parseJSON(json.slice(i, j + 1)));
+            i = j;
+            break;
+          }
+        }
+      
+
+      } else if (json[i] === ',' || json[i] === ']') {
+        curSection = json.slice(commaIndex, i);
+
+        if (curSection[0] === '"' && curSection[curSection.length - 1] === '"') {
+          resultArr.push(curSection.slice(1, curSection.length - 1));
+        } else if (+curSection) {
+          resultArr.push(+curSection);
+        } else if (curSection === 'null') {
+          resultArr.push(null);
+        } else if (curSection === 'true') {
+          resultArr.push(true);
+        } else if (curSection === 'false') {
+          resultArr.push(false);
+
+        } if (json[i] === ']') {
+          return resultArr;
+        }
+        commaIndex = i + 1;
+      } 
+    }
+  }
 };
